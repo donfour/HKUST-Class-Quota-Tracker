fc92b010a354af3065bb029d344968c9ad91a063
donfour <donovanso@yahoo.com.hk>
2018-12-26 20:04:39 +0800

    now able to query for course, server will return sorted timeseries data points

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
 
 // constants
 const MONGODB_URL = process.env.MONGODB_URL;
@@ -11,10 +12,13 @@ const PORT = 4000;
 // setup
 const app = express();
 
+// allow cors
+app.use(cors());
+
 app.use('/graphql', graphqlHTTP(req => ({
     schema,
     graphiql: true, // GUI for testing graphql server
-    rootValue: { db: req.app.locals.db }
+    rootValue: { db: req.app.locals.db } // inject db into parent object in the resolve function
 })));
 
 MongoClient.connect(MONGODB_URL) // connect to db, then start server
--- schema/schema.js
+++ schema/schema.js
@@ -1,101 +1,102 @@
-// purpose of schema.js:
-// 1) define types
-// 2)define relationships between types
-
 const graphql = require('graphql');
 
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLID,
     GraphQLInt,
     GraphQLList,
     GraphQLSchema    
 } = graphql;
 
-const books = [
-    {name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
-    {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
-    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
-    {name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
-    {name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
-    {name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'}
-];
+const TimestampType = new GraphQLObjectType({
+    name: 'Timestamp',
+    fields: () => ({
+        timestamp: { type: GraphQLString },
+        value: { type: GraphQLInt }
+    })
+})
 
-const authors = [
-    { name: 'Patrick Rothfuss', age: 44, id: '1' },
-    { name: 'Brandon Sanderson', age: 42, id: '2' },
-    { name: 'Terry Pratchett', age: 66, id: '3' }
-];
+const SectionType = new GraphQLObjectType({
+    name: 'Section',
+    fields: () => ({
+        section_id: { type: GraphQLString },
+        section_name: { type: GraphQLString },
+        wait_values: { type: new GraphQLList(TimestampType) },
+        enrol_values: { type: new GraphQLList(TimestampType) },
+        avail_values: { type: new GraphQLList(TimestampType) },
+        quota_values: { type: new GraphQLList(TimestampType) }
+    })
+})
 
-const BookType = new GraphQLObjectType({
-    name: 'Book',
-    fields: ( ) => ({
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
-        author: { // one-to-one relationship
-            type: AuthorType,
-            resolve(parent, args){
-                return authors.find(author => author.id === parent.authorId);
-            }
-        }
+const CourseType = new GraphQLObjectType({
+    name: 'Course',
+    fields: () => ({
+        course_code: { type: GraphQLString },
+        data: { type: new GraphQLList(SectionType) }
     })
 });
 
-const AuthorType = new GraphQLObjectType({
-    name: 'Author',
-    fields: ( ) => ({
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt },
-        books: { // one-to-many relationship
-            type: new GraphQLList(BookType),
-            resolve(parent, args){
-                return books.filter(book => book.authorId === parent.id);
-            }
-        }
-    })
-});
-
-// A root query can be:
-// 1) a query for one object of a type
-// 2) a query for all objects of a type
-
-// sample query:
-// book(id:'123'){
-//     name
-//     genre
-// }
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-        book: {
-            type: BookType,
-            args: { id: { type: GraphQLID } },
-            async resolve({ db }, args){
-                // code to get data from database or other source
-                console.log(await db.collection('day_data').findOne({}));
-                return books.find(book => book.id === args.id);
-            }
-        },
-        books: {
-            type: new GraphQLList(BookType),
-            resolve(parent, args){
-                return books;
-            }
-        },
-        author: {
-            type: AuthorType,
-            args: { id: { type: GraphQLID } },
-            resolve(parent, args){
-                return authors.find(book => book.id === args.id);
-            }
-        },
-        authors: {
-            type: new GraphQLList(AuthorType),
-            resolve(parent, args){
-                return authors;
+        course: {
+            type: CourseType,
+            args: { course_code: { type: GraphQLString } },
+            async resolve({ db }, { course_code }){
+                course_code = course_code.replace(/\s+/g, '').toUpperCase();
+
+                const day_section_arr = await db.collection('day_data').find({ course_code }).toArray();
+
+                // initialize an object for each section
+                const results = {}
+                day_section_arr.forEach(day_section => {
+                    if(!results.hasOwnProperty(day_section.section_id)){
+                        results[day_section.section_id] = {
+                            section_id: day_section.section_id,
+                            section_name: day_section.section_name,
+                            wait_values: [],
+                            enrol_values: [],
+                            avail_values: [],
+                            quota_values: []
+                        }
+                    }
+                });
+
+                // iterate through each day
+                day_section_arr.forEach(day => {
+                    const section_id = day.section_id;
+                    const quota_types_arr = ['avail_values', 'enrol_values', 'quota_values', 'wait_values'];
+                    
+                    quota_types_arr.forEach(quota_type => {
+                        // values were stored as objects of objects like so: { *hour*: { *minute*: *value* } }
+                        // here we transform them back to arrays of objects like so: { timestamp: *timestamp*, value: *value* }
+                        Object.keys(day[quota_type]).forEach(hour => {
+                            Object.keys(day[quota_type][hour]).forEach(minute => {
+                                const timestamp = (new Date(day.day_timestamp)).setHours(hour, minute, 0, 0);
+
+                                results[section_id][quota_type].push({
+                                    timestamp,
+                                    value: day[quota_type][hour][minute]
+                                });
+                            })
+                        })
+
+                    })
+                });
+
+                // sort all data by timestamp
+                Object.keys(results).forEach(key => {
+                    const quota_types_arr = ['avail_values', 'enrol_values', 'quota_values', 'wait_values'];
+                    quota_types_arr.forEach(quota_type => {
+                        results[key][quota_type].sort((obj1, obj2) => obj1['timestamp'] - obj2['timestamp']);
+                    })
+                })
+
+
+                return {
+                    course_code,
+                    data: Object.values(results)
+                }
             }
         }
     }
