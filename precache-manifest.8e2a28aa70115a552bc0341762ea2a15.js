ba63a5d171cdf05e89270df70cee3771f4b106a5
donfour <donovanso@yahoo.com.hk>
2019-12-31 19:34:21 +0800

    Updates

---
+++ precache-manifest.8e2a28aa70115a552bc0341762ea2a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8777234b1f0e4817df0",
+    "url": "/HKUST-Class-Quota-Tracker/static/js/runtime~main.f8777234.js"
+  },
+  {
+    "revision": "a6dcd2ea3d18e3786914",
+    "url": "/HKUST-Class-Quota-Tracker/static/js/main.a6dcd2ea.chunk.js"
+  },
+  {
+    "revision": "a7bb2c15e2f4de92359e",
+    "url": "/HKUST-Class-Quota-Tracker/static/js/1.a7bb2c15.chunk.js"
+  },
+  {
+    "revision": "a6dcd2ea3d18e3786914",
+    "url": "/HKUST-Class-Quota-Tracker/static/css/main.0305c42e.chunk.css"
+  },
+  {
+    "revision": "a7bb2c15e2f4de92359e",
+    "url": "/HKUST-Class-Quota-Tracker/static/css/1.3e9f3fc9.chunk.css"
+  },
+  {
+    "revision": "817b3a10196655e5b2345a8cb4912907",
+    "url": "/HKUST-Class-Quota-Tracker/index.html"
+  }
+];
